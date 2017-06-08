246e181c1f5544132d12ed12d79026b8c840da79
tunght1991 <tungutc91@gmail.com>
2017-06-08 14:59:59 +0700

    add load more

--- Components/App.js
+++ Components/App.js
@@ -1,23 +1,54 @@
 import React, {Component} from 'react';
-import {View, Text, ListView, Image} from 'react-native';
+import {View, Text, ListView, Image,
+        RefreshControl} from 'react-native';
 
 export default class App extends Component {
     
     constructor(props) {
         super(props);
         this.state={
+            refreshing: false,
+            page: 0,
             dataSource: new ListView.DataSource({rowHasChanged: (r1,r2)=>r1!==r2})
         }
+    }
+
+    loadNewData() {
+        this.setState({
+            refreshing: true
+        })
+
+        fetch(`http://localhost:3333/books?page=${this.state.page + 1}`)
+            .then((responsive) => responsive.json())
+            .then((responsiveJson) => {
+                if(responsiveJson.length !== 0){
+                    mang = mang.concat(responsiveJson)
+                    this.setState({
+                        dataSource: this.state.dataSource.cloneWithRows(mang),
+                        refreshing: false,
+                        page: this.state.page + 1
+                    })
+                }
+            })
+            .catch((error) => {
+                console.log(error)
+            });
     }
 
     render(){
         return (
             <ListView 
+                refreshControl={
+                    <RefreshControl 
+                        refreshing={this.state.refreshing}
+                        onRefresh={this.loadNewData.bind(this)}
+                    />
+                }
                 dataSource={this.state.dataSource}
                 renderRow={(r) => 
                     <View style={{padding:20, borderWidth:1}}>
                         <Image source={{uri: r.image}} style={{width: 70,height: 100}}/>
-                        <Text>{r.name}</Text>
+                        <Text>{r.id}</Text>
                     </View>
                 }
             />
@@ -27,16 +58,15 @@ export default class App extends Component {
 
     componentDidMount() {
         //load data
-        fetch('http://localhost:3333/books')
+        fetch(`http://localhost:3333/books?page=${this.state.page}`)
             .then((responsive) => responsive.json())
             .then((responsiveJson) => {
-                console.log(responsiveJson)
+                mang = responsiveJson
                 this.setState({
-                    dataSource: this.state.dataSource.cloneWithRows(responsiveJson)
+                    dataSource: this.state.dataSource.cloneWithRows(mang)
                 })
             })
             .catch((error) => {
-                console.log("da error")
                 console.log(error)
             });
         
